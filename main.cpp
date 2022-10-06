43f0b3b4ddd2cd4e3ae332c9a858c08c09347158
Collin Lowing <collin.lowing@protonmail.com>
2022-10-05 19:57:35 -0500

    simulation 2 added

--- main.cpp
+++ main.cpp
@@ -18,9 +18,19 @@ int main() {
     int mu = testValues.at(1);
     int M = testValues.at(2);
     int numberOfEvents = testValues.at(3);
-    Simulation sim(lambda, mu, M, numberOfEvents);
+    Simulation sim1(lambda, mu, M, numberOfEvents);
     // action
-    sim.startSimulation();
+    sim1.startSimulation();
+    testValues.clear();
+
+    testValues = parser.getTestValuesFromFile("test2.txt");
+    lambda = testValues.at(0);
+    mu = testValues.at(1);
+    M = testValues.at(2);
+    numberOfEvents = testValues.at(3);
+    Simulation sim2(lambda, mu, M, numberOfEvents);
+    // action
+    sim2.startSimulation();
 
     return 0;
 }
